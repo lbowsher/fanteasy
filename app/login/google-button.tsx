e34555d9fffac5b4a93b73e7afd84f5d504ac9dc
Luke Bowsher <lukebowsher@Lukes-MacBook-Pro.local>
2023-10-29 22:50:45 -0700

    added google logo for login

---
+++ app/login/google-button.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import Image from "next/image";
+
+export default function GitHubButton() {
+  const supabase = createClientComponentClient(); // TODO: add db type later
+
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${location.origin}/auth/callback`,
+      },
+    });
+  };
+
+  return (
+    <button onClick={handleSignIn} className="hover:bg-gray-800 p-8 rounded-xl">
+      <Image
+        src="/continue-with-google.png"
+        alt="Continue with Google"
+        width={100}
+        height={100}
+      />
+    </button>
+  );
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,21 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import GoogleButton from "./google-button";
+
+export const dynamic = "force-dynamic";
+
+export default async function Login() {
+    const supabase = createServerComponentClient({ cookies }); // TODO: add db type later
+
+    const {data : { session }} = await supabase.auth.getSession();
+    if (session) {
+        redirect('/');
+    }
+
+    // TODO: style this better
+    // TODO: probably wanna switch to personalized google sign in eventually
+    return <div className="flex-1 flex justify-center items-center">
+        <GoogleButton/>
+        </div>;
+}
